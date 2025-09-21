58b18bb315e5fac16ed833b58441d993576d381c
automate-io <naveen.351986@yahoo.com>
2025-09-21 22:19:37 +0100

    ✏️ Updated: 1.Moved the server.ts file into a subfolder

---
+++ src/server/server.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env node ;
+
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio";
+import { registerMcpTools } from "../tools/tools";
+
+// Creating MCP Server instance
+let server = new McpServer({
+    name: "API Testing MCP",
+    version: "0.0.1",
+    capabilities: {
+        resources: {},
+        tools: {},
+        propmts: {}
+    }
+});
+
+// Registering MCP tools
+server = registerMcpTools(server);
+
+/**
+ * @description Initializes and starts the MCP Server.
+ * This asynchronous function creates a new `StdioServerTransport` instance,
+ * connects the server using the provided transport, and logs a message
+ * indicating that the MCP Server is running.
+ * @author Veeranki Naveen Goud
+ */
+async function main(){
+    const transport = new StdioServerTransport()
+    await server.connect(transport);
+    console.log("API Testing MCP Server is running...");
+}
+
+// Starting the server
+main();
